c786a4afb7117282774718b8a4a00fa7b89480cf
inaimathi <leo.zovic@gmail.com>
2023-04-29 22:57:31 -0400

    Extremely basic chat stub

--- src/clj_turing_test/front_end/core.cljs
+++ src/clj_turing_test/front_end/core.cljs
@@ -4,6 +4,42 @@
             [reagent.core :as r]
             [reagent.dom :as rd]))
 
+(defonce messages (r/atom []))
+
+(defonce current-message (r/atom ""))
+
+(defn $ [selector]
+  (.querySelector js/document selector))
+
+(defn message-list [messages]
+  (->> messages
+       (map (fn [m] [:li m]))
+       (cons :ul)
+       vec))
+
+(defn send-message! []
+  (swap! messages #(conj % @current-message))
+  (reset! current-message ""))
+
+(defn game []
+  [:div {}
+   [:h3 "Welcome to"]
+   [:h1 "Turing Test!!!"]
+   [:h5 "(but from the FE though...)"]
+   (message-list @messages)
+   [:input {:type "text"
+            :value @current-message
+            :class "messageInput"
+            :on-change #(reset! current-message (.. % -target -value))
+            :onKeyDown (fn [ev]
+                         (when (= (.-keyCode ev) 13)
+                           (.preventDefault ev)
+                           (send-message!)))}]
+   [:button {:on-click send-message! } "Submit"]])
+
+(defn ^:export run []
+  (rd/render [game] ($ "body")))
+
 (.log js/console "HELLO FROM CLJS")
 
 (defn on-load [callback]
@@ -14,4 +50,5 @@
 
 (on-load
  (fn []
-   (.log js/console "DOMContentLoaded callback")))
+   (.log js/console "DOMContentLoaded callback")
+   (run)))

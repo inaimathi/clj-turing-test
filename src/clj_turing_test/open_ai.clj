6487a4097df9a79fa1e3a3dafaf7281168f98763
inaimathi <leo.zovic@gmail.com>
2023-05-19 11:21:26 -0400

    Implement basic image hooks for some reason :/

--- src/clj_turing_test/open_ai.clj
+++ src/clj_turing_test/open_ai.clj
@@ -1,5 +1,6 @@
 (ns clj-turing-test.open-ai
   (:require [clojure.string :as str]
+            [clojure.java.io :as io]
 
             [org.httpkit.client :as http]
             [org.httpkit.sni-client :as sni-client]
@@ -10,7 +11,7 @@
 
 (def API_KEY "LOLNOPE")
 
-(defn api-openai [endpoint & {:keys [body version method] :or {version "v1" method :get}}]
+(defn -api-openai [endpoint & {:keys [body version method] :or {version "v1" method :get}}]
   (let [url (str "https://api.openai.com/" version "/" endpoint)
         method (if body :post method)
         auth {"Authorization" (str "Bearer " API_KEY)}
@@ -23,10 +24,10 @@
         opts (if body (merge min-opts {:body (json/encode body)}) min-opts)]
     @(http/request opts callback)))
 
-(defn models [] (get (api-openai "models") "data"))
+(defn models [] (get (-api-openai "models") "data"))
 
 (defn completion [prompt]
-  (api-openai
+  (-api-openai
    "completions"
    :body {:model "text-davinci-003"
           :prompt prompt
@@ -34,7 +35,67 @@
           :temperature 1}))
 
 (defn chat [messages]
-  (api-openai
+  (-api-openai
    "chat/completions"
    :body {:model "gpt-3.5-turbo"
           :messages messages}))
+
+(defn -image-urls [response]
+  (map #(get % "url") (get response "data")))
+
+(defn image [prompt & {:keys [count size response-format user] :or {count 1 size 1024 response-format :url}}]
+  (assert (>= 10 count 1))
+  (assert (#{256 512 1024} size))
+  (assert (#{:url :b64_json} response-format))
+  (-image-urls
+   (-api-openai
+    "images/generations"
+    :body {:prompt prompt
+           :n count
+           :size (str size "x" size)
+           :response_format response-format})))
+
+;; (defn -api-openai-file [endpoint filename {:keys [count version size response-format user mask prompt] :or {version "v1"}}]
+;;   (let [callback (fn [{:keys [status headers body error]}]
+;;                    (if error
+;;                      (println "FAILED: " error)
+;;                      (json/decode body)))
+;;         req-props {:url (str "https://api.openai.com/" version "/images/" endpoint)
+;;                    :headers {"Authorization" (str "Bearer " API_KEY)}
+;;                    :method :post
+;;                    :multipart [{:name "n" :content (str count)}
+;;                                {:name "size" :content (str size "x" size)}
+;;                                {:name "response_format" :content (name response-format)}
+;;                                {:name "image" :content (io/file filename) :filename "test.png"}]}]
+;;     ;; (println (str req-props))
+;;     @(http/request req-props callback)))
+
+;; (defn image-edit [image prompt & {:keys [count size response-format mask user] :or {count 1 size 1024 response-format :url} :as props}]
+;;   (assert (>= 10 count 1))
+;;   (assert (#{256 512 1024} size))
+;;   (assert (#{:url :b64_json} response-format))
+;;   (-api-openai-file
+;;    "edits" image
+;;    {:mask mask
+;;     :prompt prompt
+;;     :n count
+;;     :size (str size "x" size)
+;;     :response_format response-format}))
+
+(defn image-variations [image & {:keys [count size response-format user] :or {count 1 size 1024 response-format :url} :as props}]
+  (assert (>= 10 count 1))
+  (assert (#{256 512 1024} size))
+  (assert (#{:url :b64_json} response-format))
+  (let [callback (fn [{:keys [status headers body error]}]
+                   (if error
+                     (println "FAILED: " error)
+                     (json/decode body)))
+        req-props {:url (str "https://api.openai.com/v1/images/variations")
+                   :headers {"Authorization" (str "Bearer " API_KEY)}
+                   :method :post
+                   :multipart [{:name "n" :content (str count)}
+                               {:name "size" :content (str size "x" size)}
+                               {:name "response_format" :content (name response-format)}
+                               {:name "image" :content (io/file image) :filename image}]}]
+    ;; (println (str req-props))
+    (-image-urls @(http/request req-props callback))))
